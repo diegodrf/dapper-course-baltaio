3e675c9264b804becb4fd2dcad603cc2e79eac81
Diego Faria <diego.rdfaria@gmail.com>
2022-10-06 00:52:14 -0300

    Start course project

---
+++ CourseProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace CourseProject;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var configurations = new ConfigurationBuilder()
+            .AddUserSecrets<Program>()
+            .Build();
+
+        var connectionString = configurations.GetConnectionString("Blog");
+    }
+}
+
